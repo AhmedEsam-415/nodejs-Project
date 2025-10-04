c2227a5d21a21fa558a4dcfa383ec54fa64877ec
Ahmed Esam <ahmedesam415@gmail.com>
2025-10-05 02:33:55 +0300

    تعديل في كلمه بورت

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
-const port = process.env.port || 3001;
+const port = process.env.PORT || 3001;
 
 //* Middleware أساسية
 app.use(express.urlencoded({ extended: true }));
